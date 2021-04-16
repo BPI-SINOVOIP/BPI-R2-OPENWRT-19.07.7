c8db93964eb36398fde599cd59a7d990dd85ff4c
wei.zuo <wei.zuo@radisys.com>
2021-04-16 20:00:08 +0800

    Initial Version

---
+++ target/linux/mediatek/image/mt7623.mk
@@ -0,0 +1,13 @@
+define Device/7623n-bananapi-bpi-r2-sd
+  DEVICE_TITLE := MTK7623n BananaPi R2 SD
+  DEVICE_DTS := mt7623n-bananapi-bpi-r2-sd
+endef
+
+TARGET_DEVICES += 7623n-bananapi-bpi-r2-sd
+
+define Device/7623n-bananapi-bpi-r2-emmc
+  DEVICE_TITLE := MTK7623n BananaPi R2 EMMC
+  DEVICE_DTS := mt7623n-bananapi-bpi-r2-emmc
+endef
+
+TARGET_DEVICES += 7623n-bananapi-bpi-r2-emmc
